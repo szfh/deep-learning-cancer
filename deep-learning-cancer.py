cfe06e42b19075d4f666adf86770a9e1b7ecc15a
szfh <52595717+szfh@users.noreply.github.com>
2020-04-01 15:28:55 +0100

    import dataset

--- deep-learning-cancer.py
+++ deep-learning-cancer.py
@@ -3,3 +3,16 @@
 date: 2020-03-29
 """
 
+"""
+data preprocessing
+"""
+
+# import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# import dataset
+dataset = pd.read_excel('breast-cancer.xls')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 9].values
