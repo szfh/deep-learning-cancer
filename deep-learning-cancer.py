1c351b862714405e61677a8bc60d1672902debbb
szfh <52595717+szfh@users.noreply.github.com>
2020-04-02 16:31:47 +0100

    datetime_to_string function

--- deep-learning-cancer.py
+++ deep-learning-cancer.py
@@ -37,12 +37,44 @@ X[:, 3] = np.where(X[:, 3]==datetime.datetime(2019, 11, 9), '9-11', X[:, 3])
 X[:, 3] = np.where(X[:, 3]==datetime.datetime(2014, 12, 1), '12-14', X[:, 3])
 print(np.unique(X[:, 3]))
 
+# function to replace datetimes with strings
+def datetime_to_string(s):
+    switch={
+        datetime.datetime(2014, 10, 1):'10-14',
+        datetime.datetime(2019, 9, 5):'5-9',
+        datetime.datetime(2019, 5, 3):'3-5',
+        datetime.datetime(2019, 8, 6):'6-8',
+        datetime.datetime(2019, 11, 9):'9-11',
+        datetime.datetime(2014, 12, 1):'12-14',
+        }
+    return switch.get(s,s)
+
+# print(datetime_to_string(datetime.datetime(2014, 10, 1)))
+# print(datetime_to_string(datetime.datetime(2019, 9, 5)))
+# print(datetime_to_string('0-2'))
+
 # function to get the mid point of a range of values
 def get_mid_point(n):
-# get the mid point of a range of values
     n = n.split('-')
     n = [int(i) for i in n]
     n = np.mean(n)
     n = np.ceil(n)
     n = int(n)
     return(n)
+
+# for n in X[:, 2]:
+#     print(get_mid_point(n))
+
+# try to apply the function in a for loop, assigning the value to the same cell
+a = X[:, 2]
+# for n in a:
+    # print(n)
+    # a[n] = get_mid_point(n)
+    # print(get_mid_point(n))
+
+for n in range(len(a[:20])):
+    print(a[n])
+    a[n] = get_mid_point(a[n])
+    print(a[n])
+
+# print(a)
