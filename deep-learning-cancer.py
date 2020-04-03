25c1b452441316aa353afb3815fc807f985e3bd0
szfh <52595717+szfh@users.noreply.github.com>
2020-04-03 11:55:49 +0100

    feature scaling

--- deep-learning-cancer.py
+++ deep-learning-cancer.py
@@ -98,10 +98,10 @@ X = np.delete(X,[0,5],axis=1)
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# #feature scaling
-# from sklearn.preprocessing import StandardScaler
-# sc_X = StandardScaler()
-# X_train = sc_X.fit_transform(X_train)
-# X_test = sc_X.transform(X_test)
-# sc_y = StandardScaler()
-# y_train = sc_y.fit_transform(y_train.reshape(-1,1))
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train.reshape(-1,1))
