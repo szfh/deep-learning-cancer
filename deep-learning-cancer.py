b87be0a9dfbf6df97dfee122c9da7ad6e92fecf8
szfh <52595717+szfh@users.noreply.github.com>
2020-04-05 11:06:47 +0100

    remove old code

--- deep-learning-cancer.py
+++ deep-learning-cancer.py
@@ -9,33 +9,13 @@ data preprocessing
 
 # import libraries
 import numpy as np
-# import matplotlib.pyplot as plt
 import pandas as pd
 import datetime as datetime
-
-from sklearn import preprocessing
 
 # import dataset
 dataset = pd.read_excel('breast-cancer.xls')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
-
-# encode categorical data
-# from sklearn.compose import ColumnTransformer
-# from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-# ct = ColumnTransformer([('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-# X = np.array(ct.fit_transform(X), dtype=np.float)
-# y = LabelEncoder().fit_transform(y)
-
-# replace datetimes with strings
-# X[:, 2] = np.where(X[:, 2]==datetime.datetime(2014, 10, 1), '10-14', X[:, 2])
-# X[:, 2] = np.where(X[:, 2]==datetime.datetime(2019, 9, 5), '5-9', X[:, 2])
-# print(np.unique(X[:, 2]))
-# X[:, 3] = np.where(X[:, 3]==datetime.datetime(2019, 5, 3), '3-5', X[:, 3])
-# X[:, 3] = np.where(X[:, 3]==datetime.datetime(2019, 8, 6), '6-8', X[:, 3])
-# X[:, 3] = np.where(X[:, 3]==datetime.datetime(2019, 11, 9), '9-11', X[:, 3])
-# X[:, 3] = np.where(X[:, 3]==datetime.datetime(2014, 12, 1), '12-14', X[:, 3])
-# print(np.unique(X[:, 3]))
 
 # function to replace datetimes with strings
 def datetime_to_string(s):
@@ -72,9 +52,6 @@ from sklearn.impute import SimpleImputer
 missingvalues = SimpleImputer(missing_values='?',strategy='most_frequent')
 missingvalues = missingvalues.fit(X)
 X=missingvalues.transform(X)
-
-# for j in [1,4,7,8]:
-#     print(np.unique(X[:, j], return_counts=True))
 
 # encode categorical data
 from sklearn.compose import ColumnTransformer
@@ -129,7 +106,7 @@ classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'si
 classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
 
 # fit the ann to the training set
-classifier.fit(X_train, y_train, batch_size = 10, epochs = 100)
+classifier.fit(X_train, y_train, batch_size = 10, epochs = 500)
 
 # Part 3 - Making the predictions and evaluating the model
 # predict the test set results
